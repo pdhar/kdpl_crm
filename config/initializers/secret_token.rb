14282cb33457c58cd46dc4e7468c1f97bca3570b
pdhar <pranav.dhar2@gmail.com>
2013-04-23 16:02:04 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KdplCrm::Application.config.secret_token = '34dfca73b9579c07403c349c460ba423286dab0e9057fdeca27f5b6b7f929b856982ad9e2790040fc58f08bdb2449c0c1f3d080751ea6b80b90ab42a68b2f2b9'
